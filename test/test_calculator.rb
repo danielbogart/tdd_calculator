50352225353b1c667111fff89e470621c7404b34
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-08-10 10:28:57 -0700

    First addition test passed

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -1,5 +1,10 @@
 require 'test/unit'
+require 'app/calculator' #now has access to calculator class in app folder
 
 class CalculatorTest < Test::Unit::TestCase
-
+  def test_can_add
+    calculator = Calculator.new		#calls a new Calculator from the Calculator class
+    actual = calculator.add(2, 2)
+    assert_equal(4, actual)
+  end
 end
